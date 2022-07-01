075e6464bfbed01ac3ffbf0c9329418390ced6f3
NereidaRO <nereida@DESKTOP-QQP5ADL.localdomain>
2022-07-01 13:05:58 +0200

    added collapsables

--- src/js/01-global.js
+++ src/js/01-global.js
@@ -11,13 +11,14 @@ const iconPreview = document.querySelector ('.js-iconPreview');
 //form
 //design
 
-
-
-const arrowDesign = document.querySelector ('.js-arrowDesign');
+const titleDesign = document.querySelector ('.js__design');
+const sectionDesign = document.querySelector('.js-sectionDesign');
+const arrowDesign = document.querySelector ('.js_arrowDesign');
 
 
 //fill
 
+const sectionFill = document.querySelector('.js-sectionFill');
 const titleFill = document.querySelector('.js__fill');
 const arrowFill = document.querySelector('.js_arrowFill');
 let inputName = document.querySelector('.js-inputName');
@@ -29,9 +30,9 @@ let inputLinkedin = document.querySelector('.js-inputLinkedin');
 let inputGithub = document.querySelector('.js-inputGithub');
 
 //share
+
+const contentShareOne = document.querySelector('.js__content_shareOne');
 const titleShare = document.querySelector ('.js__share');
 const arrowShare = document.querySelector('.js_arrowShare');
 const btnShare = document.querySelector('.js-btnShare');
 const contentShareTwo = document.querySelector('.js__content-shareTwo');
-
-
--- src/js/02-collapsable.js
+++ src/js/02-collapsable.js
@@ -1,22 +1,36 @@
 'use strict';
 
-const titleDesign = document.querySelector ('.js__design');
-const sectionDesign = ('.js-sectionDesign');
-const sectionFill = document.querySelector('.js-sectionFill');
-const contentShareOne = document.querySelector('.js__content_shareOne');
-
-
-
 const handleClick = (ev) => {
   const elementClick = ev.currentTarget;
   if (elementClick.classList.contains('js__design')){
-    sectionDesign.classList.toggle('hidden');}
-  else if (elementClick.classList.contains ('js__fill')){
-    sectionFill.classList.toggle('hidden');}
-  else if (elementClick.classList.contains ('js__share')){
-    contentShareOne.classList.toggle('hidden');}
+    sectionDesign.classList.toggle('hidden');
+    arrowDesign.classList.toggle('arrow-translate');
+    sectionFill.classList.add('hidden');
+    contentShareOne.classList.add('hidden');
+  }
+ else if (elementClick.classList.contains ('js__fill')){sectionFill.classList.toggle('hidden');
+  arrowFill.classList.toggle('arrow-translate');
+  sectionDesign.classList.add('hidden');
+  contentShareOne.classList.add('hidden');
+}
+
+else if (elementClick.classList.contains ('js__share')){ contentShareOne.classList.toggle('hidden');
+  arrowShare.classList.toggle('arrow-translate');
+  sectionFill.classList.add('hidden');
+  sectionDesign.classList.add('hidden');
+}
 };
 titleDesign.addEventListener ('click',handleClick);
-console.log("hola");
+titleFill.addEventListener ('click',handleClick);
+titleShare.addEventListener ('click',handleClick);
 
 
+//lo de twitter y comparte
+
+const handleClickBtn = (ev) => {
+ ev.preventDefault();
+ contentShareTwo.classList.remove('hidden')
+ btnShare.classList.add('btn-share-grey');
+};
+
+btnShare.addEventListener ('click',handleClickBtn);
