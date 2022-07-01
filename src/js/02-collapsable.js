f80d260c87ae02273ff428bbd07fd40060a570e0
NereidaRO <nereida@DESKTOP-QQP5ADL.localdomain>
2022-07-01 13:15:40 +0200

    collapsables are done

--- src/js/02-collapsable.js
+++ src/js/02-collapsable.js
@@ -7,17 +7,23 @@ const handleClick = (ev) => {
     arrowDesign.classList.toggle('arrow-translate');
     sectionFill.classList.add('hidden');
     contentShareOne.classList.add('hidden');
+    arrowFill.classList.add('arrow-translate');
+    arrowShare.classList.add('arrow-translate');
   }
  else if (elementClick.classList.contains ('js__fill')){sectionFill.classList.toggle('hidden');
   arrowFill.classList.toggle('arrow-translate');
   sectionDesign.classList.add('hidden');
   contentShareOne.classList.add('hidden');
+  arrowDesign.classList.add('arrow-translate');
+  arrowShare.classList.add('arrow-translate');
 }
 
 else if (elementClick.classList.contains ('js__share')){ contentShareOne.classList.toggle('hidden');
   arrowShare.classList.toggle('arrow-translate');
   sectionFill.classList.add('hidden');
   sectionDesign.classList.add('hidden');
+  arrowFill.classList.add('arrow-translate');
+  arrowDesign.classList.add('arrow-translate');
 }
 };
 titleDesign.addEventListener ('click',handleClick);
