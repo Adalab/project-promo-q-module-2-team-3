c1d2966adfb2cdcd2c60197e17f1680d35b0ae4f
nereazam <nerea_zama@hotmail.com>
2022-07-01 13:55:45 +0200

    add objetc

--- src/js/03-formPreview.js
+++ src/js/03-formPreview.js
@@ -12,11 +12,77 @@ const data = {
 };
 
 function handleInput(){
+namePreview.innerHTML=data.name;
 let valueName = inputName.value;
+data.name= valueName;
+};
+
+inputName.addEventListener('keyup', handleInput);
+
+function handleInput(){
+    namePreview.innerHTML=data.name;
+    let valueName = inputName.value;
+    data.name= valueName;
+    };
+    
+    inputName.addEventListener('keyup', handleInput);
+
+
+
+function handleInput(){
+    positionPreview.innerHTML=data.job;
+    let valuePosition = inputPosition.value;
+    data.job= valuePosition;
+        };
+        
+    inputPosition.addEventListener('keyup', handleInput);
+        
+        
+function handleInput(){
+    namePreview.innerHTML=data.name;
+    let valueName = inputName.value;
+    data.name= valueName;
+    };
+            
+    inputName.addEventListener('keyup', handleInput);
+            
+            
+            
+function handleInput(){
+    namePreview.innerHTML=data.name;
+     let valueName = inputName.value;
+    data.name= valueName;
+                };
+                
+     inputName.addEventListener('keyup', handleInput);
+
+
+function handleInput(){
+    namePreview.innerHTML=data.name;
+    let valueName = inputName.value;
+     data.name= valueName;
+                    };
+                    
+    inputName.addEventListener('keyup', handleInput);
+                    
+    
+    
+function handleInput(){
+    namePreview.innerHTML=data.name;
+    let valueName = inputName.value;
+ data.name= valueName;
+                        };
+                        
+ inputName.addEventListener('keyup', handleInput);
+                                                                                    
+
+
+
+
+
 let valuePosition = inputPosition.value;
-let valueImage = inputImage.value;
+//let valueImage = inputImage.value;
 let valuePhone = inputPhone.value;
 let valueEmail = inputEmail.value;
 let valueLinkedin = inputLinkedin.value;
 let valueGithub = inputGithub.value;
-};
